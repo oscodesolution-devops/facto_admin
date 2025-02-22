ba1c86bde64bd4d0090893f6fa9b48fcf7c97a69
Bifin Babu <bifinbabu088@gmail.com>
2025-02-22 12:52:09 +0530

    fix: non-existing user icon import from react-cions

--- src/components/Dashboard/dashboardCards.tsx
+++ src/components/Dashboard/dashboardCards.tsx
@@ -1,5 +1,5 @@
 import { FaMoneyCheck } from "react-icons/fa";
-import { LuUsers2, LuFileCheck2 } from "react-icons/lu";
+import { LuFileCheck2, LuUsersRound } from "react-icons/lu";
 import { RiTodoLine, RiStackLine } from "react-icons/ri";
 import { MdOutlineSlowMotionVideo } from "react-icons/md";
 import { useEffect, useState } from "react";
@@ -9,65 +9,64 @@ import { QUERIES } from "@/api/query";
 import { REQCALL } from "@/api/reqcall";
 
 interface UserType {
-  _id:string,
-  email: string,
-  fullName: string,
-  phoneNumber: string,
-  aadharNumber: string,
-  panNumber: string,
-  dateOfBirth: string
+  _id: string;
+  email: string;
+  fullName: string;
+  phoneNumber: string;
+  aadharNumber: string;
+  panNumber: string;
+  dateOfBirth: string;
 }
 
 interface ServiceType {
   _id: string;
   title: string;
   description: string;
-  icon: File | string; 
-  isActive:boolean;
+  icon: File | string;
+  isActive: boolean;
 }
 
-interface QueryType{
-  _id:string
-  email: string,
-  name: string,
-  phoneNo: number,
-  query: string,
-  createdAt?:string;
-  comment?:string
+interface QueryType {
+  _id: string;
+  email: string;
+  name: string;
+  phoneNo: number;
+  query: string;
+  createdAt?: string;
+  comment?: string;
 }
-interface ReqCallsType{
-  _id:string,
-  phoneNo:number,
+interface ReqCallsType {
+  _id: string;
+  phoneNo: number;
 }
-
 
 const DashboardCards = () => {
-
-
-//user's info
+  //user's info
   const [usersData, setUsersData] = useState<UserType[]>([]);
-  const fetchData= async()=>{
-    try{
+  const fetchData = async () => {
+    try {
       const response = await USERS.GetUsers();
       setUsersData(response.data.users);
-      console.log("front",response.data.users);
-    }catch(error){
+      console.log("front", response.data.users);
+    } catch (error) {
       console.error(error);
     }
-  }
- 
-
+  };
 
   //service info
   const [servicesData, setServicesData] = useState<ServiceType[]>([]);
-  const [activeServicesData, setActiveServicesData] = useState<ServiceType[]>([]);
+  const [activeServicesData, setActiveServicesData] = useState<ServiceType[]>(
+    []
+  );
 
   const fetchServiceData = async () => {
     try {
       const response = await SERVICES.GetServices();
       const allServices = response.data.services;
       setServicesData(allServices);
-      const activeServices = allServices.filter((service: ServiceType) => service.isActive);
+      const activeServices = allServices.filter(
+        (service: ServiceType) => service.isActive
+      );
       setActiveServicesData(activeServices);
     } catch (error) {
       console.error(error);
@@ -77,57 +76,57 @@ const DashboardCards = () => {
   //query
   const [queries, setQueries] = useState<QueryType[]>([]);
 
-  const fetchQueriesData= async ()=>{
-    try{
+  const fetchQueriesData = async () => {
+    try {
       const response = await QUERIES.Get();
       console.log(response.data.queries);
-      setQueries(response.data.queries );
-    }catch(error){
+      setQueries(response.data.queries);
+    } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   //call data
   const [callData, setCallData] = useState<ReqCallsType[]>([]);
 
-  const fetchcallData= async ()=>{
-    try{
+  const fetchcallData = async () => {
+    try {
       const response = await REQCALL.Get();
       console.log(response.data.requests);
-      setCallData(response.data.requests );
-    }catch(error){
+      setCallData(response.data.requests);
+    } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
     fetchServiceData();
     fetchQueriesData();
     fetchcallData();
-  },[])
-
+  }, []);
 
   const data = [
     {
-      icon: <LuUsers2 />,
+      icon: <LuUsersRound />,
       title: "Total Users",
-      value: usersData.length? usersData.length:0,
+      value: usersData.length ? usersData.length : 0,
       bgColor: "bg-[#FFF2E5]",
       iconColor: "text-orange-500",
     },
     {
       icon: <LuFileCheck2 />,
       title: "Active Services",
-      value: activeServicesData.length? activeServicesData.length :0,
+      value: activeServicesData.length ? activeServicesData.length : 0,
       bgColor: "bg-[#FFF2E5]",
       iconColor: "text-orange-400",
     },
     {
       icon: <LuFileCheck2 />,
       title: "Expired Services",
-      value: servicesData.length? (servicesData.length-activeServicesData.length):0,
+      value: servicesData.length
+        ? servicesData.length - activeServicesData.length
+        : 0,
       bgColor: "bg-[#FFF2E5]",
       iconColor: "text-red-600",
       textColour: "text-red-600",
@@ -142,14 +141,14 @@ const DashboardCards = () => {
     {
       icon: <RiTodoLine />,
       title: "Queries",
-      value: queries.length?queries.length:0,
+      value: queries.length ? queries.length : 0,
       bgColor: "bg-[#FFF2E5]",
       iconColor: "text-green-600",
     },
     {
       icon: <RiTodoLine />,
       title: "Call Requests",
-      value: callData.length?callData.length:0,
+      value: callData.length ? callData.length : 0,
       bgColor: "bg-[#FFF2E5]",
       iconColor: "text-green-600",
     },
@@ -180,7 +179,9 @@ const DashboardCards = () => {
             {item.icon}
           </div>
           <div className={`ml-4 ${item.textColour}`}>
-            <p className={`text-sm font-medium text-gray-800 ${item.textColour}`}>
+            <p
+              className={`text-sm font-medium text-gray-800 ${item.textColour}`}
+            >
               {item.title}
             </p>
             <p className="text-lg font-semibold">{item.value}</p>
